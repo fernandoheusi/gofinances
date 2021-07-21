f62463bf1eb71b7da75146e1af95556260c143c9
fernandoheusi <fernandoj.mhs@gmail.com>
2021-07-21 11:04:19 -0300

    Criar Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,89 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
-import  { Container, Title } from './styles';
+import  { 
+	Container ,
+	Header,
+	UserWrapper,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting, 
+	UserName,
+	Icon,
+	HighlightCards,
+	Transactions,
+	Title,
+	TrasactionList,
+} from './styles';
 
 export function Dashboard(){
+	const data = [
+	{
+		title: "Desenvolvimento do site",
+		amount: "R$ 12.000,00",
+		category: {
+			name: 'Vendas',
+			icon: 'dollar-sign'
+		},
+		date: "13/04/2020k"
+	},{
+		title: "Desenvolvimento do site",
+		amount: "R$ 12.000,00",
+		category: {
+			name: 'Vendas',
+			icon: 'dollar-sign'
+		},
+		date: "13/04/2020k"
+	},{
+		title: "Desenvolvimento do site",
+		amount: "R$ 12.000,00",
+		category: {
+			name: 'Vendas',
+			icon: 'dollar-sign'
+		},
+		date: "13/04/2020k"
+	}]
+	
 	return(
 		<Container >
-			<Title >Dashboard</Title>
-		</Container>
+			<Header>
+				<UserWrapper>
+					<UserInfo>
+						<Photo 
+							source={{ uri: 'https://avatars.githubusercontent.com/u/63732638?v=4'}}
+						/>
+						
+						<User>
+							<UserGreeting>Olá</UserGreeting>
+							<UserName>Fernando</UserName>
+						</User>
+					</UserInfo>
+
+					<Icon name="power"/>
+				</UserWrapper>
+			</Header>
+
+			<HighlightCards>
+				<HighlightCard type="up" title="Entradas" amount="R$ 17.400,00" lastTransaction="Última entrada dia 13 de abril"/>
+				<HighlightCard type="down" title="saidas" amount="R$ 1.259,00" lastTransaction="Última saída dia 03 de abril"/>
+				<HighlightCard type="total" title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril"/>
+			</HighlightCards>
+
+			<Transactions>
+				<Title>Listagem</Title>
+
+				<TrasactionList 
+					data={data}
+					renderItem={({ item }) => <TransactionCard data={item}/>}
+					showsVerticalScrollIndicator={false}
+					contentContainerStyle={{
+						paddingBottom: getBottomSpace()
+					}}
+				/>
+			</Transactions>
+		</ Container>
 	)
 }
